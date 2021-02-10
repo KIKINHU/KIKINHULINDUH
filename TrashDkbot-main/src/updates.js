8556b4c0e0bfea0c71325ae3e7a24372aad10b9f
KIKINHU <kikogato211@gmail.com>
2021-02-10 09:23:51 -0300

    First commit

---
+++ TrashDkbot-main/src/updates.js
@@ -0,0 +1,31 @@
+const updates = (prefix, pushname) => {
+ return `
+Novos comandos atualizados pelo meu dono 
+novos comandos :  
+
+${prefix}Ytmp4
+
+${prefix}Setppbot
+
+${prefix}Blocklist 
+
+${prefix}wame
+
+${prefix}image
+
+${prefix}loli(refeito funcionando)
+
+${prefix}grupo fechar
+
+${prefix}grupo abrir
+
+
+
+
+digite ${prefix}help pra ver os comandos com mais detalhes
+ `
+
+
+}
+
+exports.updates = updates
